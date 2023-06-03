03c1b0fe74b27dcce1e85fcfe83039a628f3b163
Handsome Victor <857730556@qq.com>
2023-06-03 09:20:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from main_openai import main_openai
 
 if __name__ == '__main__':
     params = dict(api_key="Your API key",
-                  text_you_want_to_send="Hi, I have a friend, her name is Suzy. She is super smart, "
+                  text_you_want_to_send="Hi, I have a friend, her name is Victor. He is super smart, "
                                         "sharp and beautiful. Use French, English, Russian and Chinese "
-                                        "to compliment her. She is a great friend. Use only one "
+                                        "to compliment him. She is a great friend. Use only one "
                                         "sentence for each language.",
                   max_tokens=1500,  # only change it if your input and output are too long
                   save_format='md')  # can be 'md' or 'txt' or anything you think is convenient
